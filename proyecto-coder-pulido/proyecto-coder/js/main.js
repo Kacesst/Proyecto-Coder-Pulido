b0debba08d1b0b25aeeb3d79b6d7bb9e5f420d7f
Kacesst <124521321+Kacesst@users.noreply.github.com>
2023-04-14 16:53:28 -0300

    actualizacion

---
+++ proyecto-coder-pulido/proyecto-coder/js/main.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const nav = document.querySelector("#nav");
+  const abrir = document.querySelector("#abrir");
+  const cerrar = document.querySelector("#cerrar");
+
+  abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+  });
+
+  cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+  });
+});
+
+$(document).ready(function(){
+    $('.autoplay').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  });
+
+
+  const carousel = new bootstrap.Carousel('#myCarousel')
