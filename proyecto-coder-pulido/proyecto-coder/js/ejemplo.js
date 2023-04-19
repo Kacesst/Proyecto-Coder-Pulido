df9ce148103ca2556f8d0245da83dd7facf334e9
Kacesst <kevin.csst.grego@gmail.com>
2023-04-18 23:14:14 -0300

    dale

--- proyecto-coder-pulido/proyecto-coder/js/ejemplo.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function() {
-    var container = document.querySelector(".container");
-    var position = container.getBoundingClientRect().top;
-    var screenPosition = window.innerHeight;
-  
-    if (position < screenPosition) {
-      container.classList.add("show");
-    }
-  });
